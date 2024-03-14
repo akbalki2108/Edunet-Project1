5443dd139638ad0c91b2f103d317eb9e0e40dcfd
akbalki2108 <akbalki208012@gmail.com>
2024-03-14 13:28:11 +0530

    initial commit

--- edunet/settings.py
+++ edunet/settings.py
@@ -81,31 +81,26 @@ WSGI_APPLICATION = "edunet.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "ENGINE": "django.db.backends.postgresql",
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
 #         'OPTIONS': {
-#             'timeout': 60, 
-#             'check_same_thread': False, 
-#             'uri': True,
+#             'sslmode': os.getenv('DB_SSL_MODE', 'require'),  # Use 'require' if not specified
 #         },
 #     }
 # }
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-        'OPTIONS': {
-            'sslmode': os.getenv('DB_SSL_MODE', 'require'),  # Use 'require' if not specified
-        },
-    }
-}
 
 # admin_restaurantlyy
 # set DB_PASSWORD = restaurantlyy@123
